0c2937aa004b4ca71ca3c7b501a8d735cfd80e6a
ThyenKhuc <92849623+ThyenKhuc@users.noreply.github.com>
2022-08-21 20:24:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/thptnguyenvancuinhanoi";
+            window.location = "https://www.facebook.com/Thyen.Khuc.545/";
           },
         });
       }
