42862175d1fa7222bbd81affb9b024bbb0128a3e
ThyenKhuc <92849623+ThyenKhuc@users.noreply.github.com>
2024-02-28 21:51:30 +0700

    sửa fb

--- js/myJs.js
+++ js/myJs.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/kiekhuc.585/";
+            window.location = "https://www.facebook.com/kiekhh/";
           },
         });
       }
